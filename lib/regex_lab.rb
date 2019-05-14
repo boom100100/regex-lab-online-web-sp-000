c8e506106635df0af26e8194a80efbe57468c38b
Bernadette Davis <bsd231@nyu.edu>
2019-05-14 13:01:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,7 +27,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]\w+[?!\.]/)
+  if text[0].match(/^[A-Z]/) && text[text.length-1].match(/[\?!\.]/)
     return true
   else
     return false
