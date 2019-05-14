7d13e2a299d99686a31fef47a57800c9b707b3d4
Bernadette Davis <bsd231@nyu.edu>
2019-05-14 12:57:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,37 @@
 def starts_with_a_vowel?(word)
+  if word.match(/^[aeiouAEIOU]/)
+    return true
+  else
+    return false
+  end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un\w+ing/)
 
 end
 
 def words_five_letters_long(text)
+  finalArray = []
+
+  text = text.scan(/\w+/)
+  text.each do |word|
+    if word.length == 5
+      finalArray << word
+    end
+  end
+
+  finalArray
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].+[?!\.]/)
+    return true
+  else
+    return false
+  end
 
 end
 
